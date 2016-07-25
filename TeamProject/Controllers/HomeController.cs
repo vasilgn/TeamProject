b05e4d7e8893765068c9e63ccd37a16ab7aa3269
LenchoPetrov <lencho.pet@mail.bg>
2016-07-25 14:58:24 +0300

    Mes

---
+++ TeamProject/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamProject.DataModels;
+using System.Data.Entity;
+
+namespace TeamProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+			
+            return View();
+        }
+
+    }
+}
---
+++ TeamProject/DataModels/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TeamProject.DataModels
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IDbSet<Post> Posts { get; set; }
+        public IDbSet<Comment> Comments { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
