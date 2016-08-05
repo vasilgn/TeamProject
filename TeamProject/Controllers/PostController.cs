1ea4225103fca7cc7584260b8ff69abe8eb46710
vasilgn <vasilgn86@gmail.com>
2016-08-05 09:19:07 +0300

    add some changes

---
+++ TeamProject/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace TeamProject.Controllers
+{
+  
+    public class PostController : BaseController
+    {
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Post()
+        {
+            return PartialView("Posts");
+        }
+
+
+
+        #region Helpers
+
+
+
+        #endregion
+
+
+    }
+}
