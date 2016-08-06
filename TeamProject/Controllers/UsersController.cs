eecb53debe69ed312ec8f7946b70f3a3cc46bc8a
LenchoPetrov <lencho.pet@mail.bg>
2016-08-06 08:38:19 +0300

    Users edit, delete, details

--- TeamProject/Controllers/UsersController.cs
+++ TeamProject/Controllers/UsersController.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using TeamProject.DataModels;
 using TeamProject.Models;
+using System.Net;
+using System.Data.Entity.Infrastructure;
 
 namespace TeamProject.Controllers
 	{
@@ -41,5 +43,111 @@ namespace TeamProject.Controllers
 				}
 			return View(users.ToList());
 			}
-		}
+
+
+		//Details
+		public ActionResult Details(int? id)
+			{
+			if (id == null)
+				{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+				}
+			UsersModel user = db.ApplicationUser.Find(id);
+			if (user == null)
+				{
+				return HttpNotFound();
+				}
+			return View(user);
+			}
+
+
+		//Edit
+		public ActionResult Edit(int? id)
+			{
+			if (id == null)
+				{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+				}
+			UsersModel user = db.ApplicationUser.Find(id);
+			if (user == null)
+				{
+				return HttpNotFound();
+				}
+			return View(user);
+			}
+
+		//  User/Edit/
+		[HttpPost, ActionName("Edit")]
+		[ValidateAntiForgeryToken]
+		public ActionResult EditPost(int? id)
+			{
+			if (id == null)
+				{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+				}
+			var userToUpdate = db.ApplicationUser.Find(id);
+			if (TryUpdateModel(userToUpdate, "",
+			   new string[] { "UserName", "FullName", "PasswordHash", "Email" }))
+				{
+				try
+					{
+					db.SaveChanges();
+
+					return RedirectToAction("Index");
+					}
+				catch (RetryLimitExceededException /* dex */)
+					{
+					//Log the error (uncomment dex variable name and add a line here to write a log.
+					ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+					}
+				}
+			return View(userToUpdate);
+			}
+	
+	  // User/Delete
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+			UsersModel user = db.ApplicationUser.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                UsersModel user = db.ApplicationUser.Find(id);
+				db.ApplicationUser.Remove(user);
+                db.SaveChanges();
+            }
+            catch (RetryLimitExceededException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }	
 }
--- TeamProject/Models/UsersModel.cs
+++ TeamProject/Models/UsersModel.cs
@@ -13,7 +13,7 @@ namespace TeamProject.Models
 		public string FullName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        
+        public string PasswordHash { get; set; }
 	    
       
     }
